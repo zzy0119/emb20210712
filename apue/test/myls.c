18349b1acd5d1f34f5e7cf306cfbb370d0dba7d0
Your Name <you@example.com>
2021-08-27 17:17:19 +0800

    mygetline

--- apue/test/myls.c
+++ apue/test/myls.c
@@ -8,7 +8,9 @@
 #include <pwd.h>
 #include <time.h>
 
-#define BUFSIZE	32
+#define BUFSIZE		32
+#define LISTSIZE	256
+#define NAMESIZE	64
 
 char typeFile(mode_t mode);
 char *modeFile(mode_t mode);
@@ -18,10 +20,11 @@ char *fileOwner(uid_t uid);
 char *fileMTime(struct timespec mt);
 long fileSize(off_t size);
 long fileInode(ino_t ino);
+int showLine(const char *path, int lflag, int iflag);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-
+	showLine("./mygetline.c", 0, 0);
 
 	return 0;
 }
@@ -68,7 +71,7 @@ char *modeFile(mode_t mode)
 	// if error
 	strcpy(ret, "rwxrwxrwx");
 	for (int i = 0, j = 0; i < 9; i++) {
-		if (!modes[i] & mode)
+		if (!(modes[i] & mode))
 			ret[i] = '-';
 		else if (i % 3 == 2 && mode & modes[i]) {
 			if (mode & spc[j])
@@ -126,6 +129,50 @@ char *fileMTime(struct timespec mt)
 long fileInode(ino_t ino)
 {
 	return ino;
+}
+
+int showLine(const char *path, int lflag, int iflag)
+{
+	struct stat res;
+	char *r;
+	char buf[LISTSIZE] = {};
+	char name[NAMESIZE] = {};
+	char tmp[BUFSIZE] = {};
+
+	if (stat(path, &res) == -1) {
+		perror("stat()");
+		return -1;
+	}
+	r = strrchr(path, '/');
+	if (r == NULL)
+		strcpy(name, path);
+	else
+		strcpy(name, r+1);
+
+	if (lflag == 0 && iflag == 0) {
+		puts(name);
+		return 0;
+	}
+	if (iflag) {
+		snprintf(buf, LISTSIZE, "%ld ", res.st_ino);
+	} 
+	if (lflag) {
+		snprintf(tmp, BUFSIZE, "%c", typeFile(res.st_mode));
+		strcat(buf, tmp);
+		strcat(buf, modeFile(res.st_mode));
+		snprintf(tmp, BUFSIZE, " %ld ", linkCnt(res.st_nlink));
+		strcat(buf, tmp);
+		strcat(buf, fileOwner(res.st_uid));
+		strcat(buf, " ");
+		strcat(buf, groupName(res.st_gid));
+		snprintf(tmp, BUFSIZE, " %ld ", fileSize(res.st_size));
+		strcat(buf, tmp);
+		strcat(buf, fileMTime(res.st_mtim));
+		strcat(buf, " ");
+	}
+
+	strcat(buf, name);
+	puts(buf);
 }
 
 
